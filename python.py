aa90a56c1a02afc5b2009e336530954a231d2bd1
Armand9999 <82055216+Armand9999@users.noreply.github.com>
2021-04-06 14:38:40 -0400

    Create python.py

---
+++ python.py
@@ -0,0 +1,12 @@
+class Class:
+  def __init__(self):
+    
+  def function(self, x):
+    y = self.x.split()
+    count = 0
+    dict = {}
+    for word in y:
+      if word.isalpha():
+        count = y.count(word)
+        dict[word] = count
+   return dict
