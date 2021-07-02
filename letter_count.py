87a5f7e9bd95c01b27871423070afca77480f726
armand99 <armandnzeyang@yahoo.com>
2021-07-02 10:44:39 -0400

    Name changed from python.py to letter_count.py

---
+++ letter_count.py
@@ -0,0 +1,12 @@
+class Class:
+  def __init__(self, x):
+    sefl.x = x
+  def function(self):
+    y = self.x.split()
+    count = 0
+    dict = {}
+    for word in y:
+      if word.isalpha():
+        count = y.count(word)
+        dict[word] = count
+   return dict
